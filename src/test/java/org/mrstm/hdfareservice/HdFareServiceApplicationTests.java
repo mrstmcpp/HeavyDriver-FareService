ee1e32a8a96a9322de89942465de68d20d534c89
Satyam Prajapati <mrstm.cpp@gmail.com>
2025-10-29 15:40:42 +0530

    Started working on seperate fare microservice

---
+++ src/test/java/org/mrstm/hdfareservice/HdFareServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mrstm.hdfareservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdFareServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
